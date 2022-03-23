e3e644931af2f80cbb7da28ce076c2eda2d9a0fb
Hitesh Kumar <hiteshkumaragra00@gmail.com>
2022-03-23 14:45:03 +0530

    make Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+//Importing Components
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import MiddlePage from "./components/RestroPage/MiddlePage";
+import { Component } from "react";
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MiddlePage/>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header/TabOptions/TabOptions.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './TabOptions.css';
+import TabOption from './TabOption/TabOption';
+export class TabOptions extends Component {
+  
+  constructor()
+  {
+      super();
+      this.options = [
+          {
+              name: "Help"
+          },
+          {
+            name: "Offers"
+          },
+          {
+            name: "Sign In"
+          },
+          {
+            name: "Cart"
+          }];
+
+  }  
+  render() {
+    return <div className='tab_options'>
+        {this.options.map((option)=>
+        {    
+        return <TabOption details={option}/>
+        })}
+    </div>;
+  }
+}
+
+export default TabOptions;
