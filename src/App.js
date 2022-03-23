0b740210fe1178ee9b1853aa499ef76ae38f9f2a
Hitesh Kumar <hiteshkumaragra00@gmail.com>
2022-03-23 15:27:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 
 //Importing Components
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import MiddlePage from "./components/RestroPage/MiddlePage";
 import { Component } from "react";
 
 
@@ -13,9 +10,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <MiddlePage/>
-        {/* <Footer /> */}
       </div>
     );
   }
