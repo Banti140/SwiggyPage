3d855cbdc8d72086ea87d6ac2e048e3ea009a02a
Hitesh Kumar <hiteshkumaragra00@gmail.com>
2022-03-23 15:31:16 +0530

    added haeder and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 
 //Importing Components
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import { Component } from "react";
 
 
@@ -10,6 +12,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Footer />
       </div>
     );
   }
